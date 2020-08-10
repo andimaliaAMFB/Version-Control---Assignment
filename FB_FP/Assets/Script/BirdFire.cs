1fbc5de9edfc5e3cab5b9f78bbac7035f88c2881
Andi_malia010710 <011007andimalia@gmail.com>
2020-08-10 10:11:09 +0700

    no message

---
+++ FB_FP/Assets/Script/BirdFire.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic ;
+using UnityEngine;
+
+public class BirdFire : MonoBehaviour
+{
+    //Global variables
+    public float speed = 10f;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb.velocity = transform.right * speed*Time.deltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Pipe pipe = hitInfo.GetComponent<Pipe>();
+        Destroy(gameObject);
+    }
+}
